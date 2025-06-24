e645f43f901f199f99237fd129953bcee973ca69
Parthiban <81295901+Parthiban-Prince@users.noreply.github.com>
2025-06-24 18:42:40 +0530

    Update MessagePage.jsx
    Delete the testing components

--- src/Pages/MessagePage/MessagePage.jsx
+++ src/Pages/MessagePage/MessagePage.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import LeftSidebar from '../../Component/Tetsing/left';
-import RightSidebar from '../../Component/Tetsing/right'
 import MessageCenter from '../../Component/Messagecenter/Messagecenter';
 import SideNavigation from '../../Component/SideNavigation/SideNavigation';
 import MessageRight from '../../Component/MessageRight/MessageRight';
